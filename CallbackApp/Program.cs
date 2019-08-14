46c7450a5a890080892bafa941411c0d14dcfdaf
Abdul Ashad <AAbdul@acsind.com>
2019-08-15 03:31:28 +0530

    Add project files.

---
+++ CallbackApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.LongRunning(Print);
+        }
+
+        static void Print(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public class MyClass
+    {
+        public delegate void Callback(int i);
+
+        public void LongRunning(Callback obj)
+        {
+            for(int i = 0;i < 10000;i++)
+            {
+                obj(i);
+            }
+        }
+    }
+}
